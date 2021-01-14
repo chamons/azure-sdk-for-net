758c9e0353ac621aced0aac779dd2cc926f32037
Chris Hamons <chris.hamons@xamarin.com>
2021-01-14 11:07:39 -0600

    First set of new-style tests

---
+++ sdk/synapse/Azure.Analytics.Synapse.Artifacts/tests/DataFlowClientLiveTest.cs
@@ -0,0 +1,88 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Analytics.Synapse.Artifacts;
+using Azure.Analytics.Synapse.Artifacts.Models;
+using Azure.Core.TestFramework;
+using NUnit.Framework;
+
+namespace Azure.Analytics.Synapse.Tests
+{
+    public class DataFlowClientLiveTest : RecordedTestBase<SynapseTestEnvironment>
+    {
+        public DataFlowClientLiveTest(bool isAsync) : base(isAsync)
+        {
+        }
+
+        private DataFlowClient CreateClient()
+        {
+            return InstrumentClient(new DataFlowClient(
+                new Uri(TestEnvironment.EndpointUrl),
+                TestEnvironment.Credential,
+                InstrumentClientOptions(new ArtifactsClientOptions())
+            ));
+        }
+
+        [Test]
+        public async Task GetDataFlows()
+        {
+            DataFlowClient client = CreateClient();
+            await using DisposableDataFlow flow = await DisposableDataFlow.Create (client, this.Recording);
+
+            AsyncPageable<DataFlowResource> dataFlows = client.GetDataFlowsByWorkspaceAsync ();
+            Assert.GreaterOrEqual((await dataFlows.ToListAsync()).Count, 1);
+        }
+
+        [Test]
+        public async Task GetDataFlow()
+        {
+            DataFlowClient client = CreateClient();
+            await using DisposableDataFlow flow = await DisposableDataFlow.Create (client, this.Recording);
+
+            DataFlowResource dataFlow = await client.GetDataFlowAsync (flow.Name);
+            Assert.AreEqual (flow.Name, dataFlow.Name);
+        }
+
+        [Test]
+        public async Task RenameDataFlow()
+        {
+            DataFlowClient client = CreateClient();
+
+            // Non-disposable as we'll rename it underneath (and have to clean up ourselves)
+            DataFlowResource resource = await DisposableDataFlow.CreateResource (client, this.Recording);
+
+            string newFlowName = Recording.GenerateAssetName("DataFlow2");
+
+            DataFlowRenameDataFlowOperation renameOperation = await client.StartRenameDataFlowAsync (resource.Name, new ArtifactRenameRequest () { NewName = newFlowName } );
+            await renameOperation.WaitForCompletionAsync ();
+
+            DataFlowResource dataFlow = await client.GetDataFlowAsync (newFlowName);
+            Assert.AreEqual (newFlowName, dataFlow.Name);
+
+            DataFlowDeleteDataFlowOperation operation = await client.StartDeleteDataFlowAsync (newFlowName);
+            await operation.WaitForCompletionAsync ();
+        }
+
+        [Test]
+        public async Task DeleteDataFlow()
+        {
+            DataFlowClient client = CreateClient();
+
+            // Non-disposable as we'll be deleting it ourselves
+            DataFlowResource resource = await DisposableDataFlow.CreateResource (client, this.Recording);
+
+            DataFlowDeleteDataFlowOperation operation = await client.StartDeleteDataFlowAsync (resource.Name);
+            Response response = await operation.WaitForCompletionAsync ();
+            switch (response.Status) {
+                case 200:
+                case 204:
+                    break;
+                default:
+                    Assert.Fail($"Unexpected status ${response.Status} returned");
+                    break;
+            }
+        }
+    }
+}
--- sdk/synapse/Azure.Analytics.Synapse.Monitoring/tests/MonitoringClientLiveTests.cs
+++ sdk/synapse/Azure.Analytics.Synapse.Monitoring/tests/MonitoringClientLiveTests.cs
@@ -13,28 +13,17 @@ namespace Azure.Analytics.Synapse.Tests
 {
     public class MonitoringClientLiveTests : RecordedTestBase<SynapseTestEnvironment>
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MonitoringClientLiveTests"/> class.
-        /// </summary>
-        /// <param name="isAsync">A flag used by the Azure Core Test Framework to differentiate between tests for asynchronous and synchronous methods.</param>
         public MonitoringClientLiveTests(bool isAsync) : base(isAsync)
         {
         }
 
-        [Test]
-        public async Task TestListSparkApplications()
+        private MonitoringClient TriggerRunClient()
         {
-            SparkJobListViewResponse sparkJobList = await MonitoringClient.GetSparkJobListAsync();
-            Assert.NotNull(sparkJobList);
-            CollectionAssert.IsNotEmpty(sparkJobList.SparkJobs);
-        }
-
-        [Test]
-        public async Task TestSqlQuery()
-        {
-            SqlQueryStringDataModel sqlQuery = await MonitoringClient.GetSqlJobQueryStringAsync();
-            Assert.NotNull(sqlQuery);
-            Assert.IsNotNull(sqlQuery.Query);
+            return InstrumentClient(new MonitoringClient(
+                new Uri(TestEnvironment.EndpointUrl),
+                TestEnvironment.Credential,
+                InstrumentClientOptions(new MonitoringClientOptions())
+            ));
         }
     }
 }
