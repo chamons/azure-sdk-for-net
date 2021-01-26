eaa76a086fcb13997a4aa91728cfa3d4d706131e
Chris Hamons <chris.hamons@xamarin.com>
2021-01-26 10:24:33 -0600

    Move to sub test instead

--- sdk/textanalytics/Azure.AI.TextAnalytics/tests/Dynamic.cs
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/Dynamic.cs
@@ -1,21 +1,21 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using NUnit.Framework;
+using Azure.Core;
 
 namespace Azure.AI.TextAnalytics.Dynamic
 {
-    public class UnitTest1
+    public class Dynamic
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            dynamic d = DynamicJson.Object();
+            d.Foo = "Bar";
+            d.Buzz = 42;
+            Console.WriteLine (d);
         }
     }
 }
---
+++ sdk/textanalytics/Azure.AI.TextAnalytics/tests/run.sh
@@ -0,0 +1,1 @@
+dotnet test --framework="net5.0" /m:1 -v=normal --no-build --nologo --filter=Dynamic
